19c244ef69409b11cde09797635a093dc784c196
Ryan Small <ryan.small.work@gmail.com>
2022-09-10 23:59:43 -0400

    Update webscrape.py

--- WebScrape.py
+++ WebScrape.py
@@ -1,1 +1,17 @@
-# Py script for web scraping
+# Py script for## web scraping
+
+# Load libraries
+
+# Load Dataset
+
+# Clean data
+
+# separate into training, test, verification sets
+
+# train
+
+# test
+
+# verify with holdout
+
+# visualize
