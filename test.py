a10e03722f84e2f2610366940b91c5bacfda290c
Ryan Small <ryan.small.work@gmail.com>
2022-09-12 22:43:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-# test data
+# Load modules to obtain and manipulate data 
 Import pandas as pd
+Import json as json
+
+# Load WW_trends and US_trends data into the the given variables respectively
+WW_trends = ...
+US_trends = ... 
+
+# Inspecting data by printing out WW_trends and US_trends variables
+# CODE FOR TASK 1
+
+Print(WW_trends)
+
+
+
 
