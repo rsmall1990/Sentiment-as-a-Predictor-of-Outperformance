7979569146f673a9e1d916c7883bee1a9c01c88b
Ryan Small <ryan.small.work@gmail.com>
2022-09-15 23:12:05 -0400

    Update instructions in tweets.py

--- tweets.py
+++ tweets.py
@@ -1,6 +1,9 @@
+# Load packages for analysis
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
+# set query constraints using a scrape. This
+# still needs to be modified to loop for some list of stocks
 query = "(from:elonmusk) until:2020-01-01 since:2010-01-01"
 tweets = []
 limit = 5000
